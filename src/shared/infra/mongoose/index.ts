8e6ace6ad72e9c4f8c2257b8dbbf5c729af25c9a
Rafael Leveske <rafaelleveske@gmail.com>
2021-02-06 15:13:23 -0300

    Refatorando variaveis de ambiente

--- src/shared/infra/mongoose/index.ts
+++ src/shared/infra/mongoose/index.ts
@@ -1,8 +1,11 @@
 import mongoose from 'mongoose';
 
-mongoose.connect(`mongodb://localhost:27017/${process.env.MONGO_DB_URL}`, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+mongoose.connect(
+  `mongodb://localhost:27017/${process.env.MONGO_DB_DATABASE_NAME}`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  },
+);
