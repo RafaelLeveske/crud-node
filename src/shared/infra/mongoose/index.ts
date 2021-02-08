eb85f938d191b266a881acd5bcf39dc9e73385a3
Rafael Leveske <rafaelleveske@gmail.com>
2021-02-08 09:36:59 -0300

    Protegendo uri de conexão com o banco

--- src/shared/infra/mongoose/index.ts
+++ src/shared/infra/mongoose/index.ts
@@ -8,7 +8,7 @@ const {
 } = process.env;
 
 mongoose.connect(
-  'mongodb+srv://rafael:iYPebzZp1uFrtsmA@cluster0.a7sz7.mongodb.net/crudnode?retryWrites=true&w=majority',
+  `mongodb+srv://${MONGO_DB_USER}:${MONGO_DB_PASS}@${MONGO_DB_HOST}.mongodb.net/${MONGO_DB_DATABASE_NAME}?retryWrites=true&w=majority`,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
